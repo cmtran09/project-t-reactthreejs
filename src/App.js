cc48f911df7018620afdffe5369d77203cacee01
cmtran09 <cmtran@hotmail.co.uk>
2020-08-20 15:12:08 +0100

    ⬛️ SpinningBoxcompoent deconstructed

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import { Canvas, useFrame } from 'react-three-fiber'
 
 // import { Box } from 'drei'
 
-const Box = () => {
+const SpinningBox = ({ position, color, args }) => {
   const mesh = useRef(null)
   //adding 0.01 to x and y rotation each frame
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh ref={mesh} >
+    <mesh position={position} ref={mesh} >
       <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-      <meshStandardMaterial attach='material' />
+      <meshStandardMaterial attach='material' color='pink' />
     </mesh >
   )
 }
@@ -21,8 +21,12 @@ const Box = () => {
 function App() {
   return (
     <>
-      <Canvas>
-        <Box />
+      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+        // ambientLight globally illuminating all objects on the sceen equally
+        <ambientLight intenisty={0.3} />
+        <SpinningBox position={[0, 1, 0]} />
+        <SpinningBox position={[-2, 1, -5]} />
+        <SpinningBox position={[5, 1, -2]} />
       </Canvas>
     </>
   );
